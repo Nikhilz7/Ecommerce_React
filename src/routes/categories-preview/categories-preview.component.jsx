fa75887e13b35062cdf1acd1feb1645d871cb93b
Nikhil <nikhilc71@gmail.com>
2023-01-03 13:37:12 +0530

    Implemented Asynchronous behaviour using redux-thunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,18 +1,29 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
+
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/category.selector'
+
 import CategoryPreview   from '../../components/category-preview/category-preview.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector'
+import Spinner from '../../components/spinner/spinner.component';
+
 
 const CategoriesPreview = () => {
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
+
     return(
         <Fragment>
-            {   
-                Object.keys(categoriesMap).map((title) => {
-                    //object.keys gives an array of all the keys in categories like sneakers,hat,..
-                    const products = categoriesMap[title];
-                    return <CategoryPreview key={title} title={title} products={products}/>
-                })
+            { 
+                isLoading?(
+                    <Spinner />
+                ):(
+                    Object.keys(categoriesMap).map((title) => {
+                        //object.keys gives an array of all the keys in categories like sneakers,hat,..
+                        const products = categoriesMap[title];
+                        return <CategoryPreview key={title} title={title} products={products}/>
+                    })
+                    
+                )
             }
         </Fragment>
     );
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -63,6 +63,7 @@ export const getCategoriesAndDocuments = async () => {
     const collectionRef = collection(db, 'categories');
     const q = query(collectionRef);
 
+    await Promise.reject(new Error('Cannot find categories'));
     const querySnapshot = await getDocs(q);
     return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
         
